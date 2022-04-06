2012433c0b022054f046e8326f22b69f52d43a40
Manuel Sierra munoz <msierra-@c2r12s3.42madrid.com>
2022-04-06 20:22:05 +0200

    asd

--- includes/minishell.h
+++ includes/minishell.h
@@ -7,7 +7,7 @@
 
 typedef struct s_tree
 {
-	char			*string; // todo el string de info
+	char			*cmd; // todo el string de info
 	struct s_tree	*left; // aqui ira la info del comando
 	struct s_tree	*right; // aqui iran los pipes y se ira ramificando en funcion del numOfPipes
 } t_tree;
@@ -16,8 +16,6 @@ typedef struct s_cmd
 {
 	char			*cmd[2]; // Si solo hay 1 cmd el 2 es NULL (hay que rellenarlo);
 	struct s_cmd	*next;
-	
-
 } t_cmd;
 
 
@@ -39,9 +37,8 @@ typedef struct s_dataInfo
 	char	*outFile;
 	char	*inFile; // Implementar el GNL (Ver errores que pueda haber en los infiles)
 	char	*errFile;
-	char	**env; // Si nos la borran la guardamos en la data
-	char	**path;
 	char	**env;
+	char	**path;
 
 
 } t_dataInfo;
