978a20e95631e9aa89761e384921a46241c2427b
mbarylak <miguelbarylak@gmail.com>
2022-04-12 13:13:46 +0200

    Idk what im doin

--- includes/minishell.h
+++ includes/minishell.h
@@ -13,10 +13,17 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "./lexer.h"
+
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
 # include "readline/history.h"
 # include "readline/readline.h"
 
