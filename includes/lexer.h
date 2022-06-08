f695f67afe875881db3be23908d2c073f6f7f248
ManuelSierra95 <manuelsierramunoz@gmail.com>
2022-06-08 13:02:00 +0200

    Mergeando lo nuevo

--- includes/lexer.h
+++ includes/lexer.h
@@ -3,20 +3,21 @@
 
 enum char_type
 {
-	CHAR = -1,
+	CHAR = 1,
 	DOLLAR = '$',
 	PIPE = '|',
 	GREATER = '>',
 	LESSER = '<',
-	S_QUOTE = 39, // Comilla simple
-	D_QUOTE = '"', // Comilla doble
+	SPACE = 32,
+	S_QUOTE = 39,
+	D_QUOTE = '"',
 };
 
 enum token_type
 {
-	T_PIPE,
-	T_CMD,
+	T_CMD = 1,
 	T_TEXT,
+	T_PIPE,
 	T_GREATER,
 	T_GREATERGREATER,
 	T_LESSER,
@@ -30,8 +31,19 @@ typedef struct s_token
 } t_token;
 
 
+int		check_access(char *input);
+void	get_path(void);
+char 	**split_input(char *input);
+t_token	*lexer(char **input);
+int		quote_analyzer(char *input);
 int		check_cmd(char *input);
 int		ft_isalpha(int c);
-int		ft_isspecial(int c, int d);
+int		d_isspecial(int c, int d);
+int		s_isspecial(int c);
+char	*take_off_quotes(char *input);
+int		check_quotes(char *input);
+int		count_quotes(char *input);
+int		input_state(char input);
 
+void	check_quote_error(char *input);
 #endif
